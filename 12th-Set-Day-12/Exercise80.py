5d8bbcb936344a9da3d8068635d13ecb9f8889f8
ChrisCh9 <151587068+ChrisCh9@users.noreply.github.com>
2024-03-03 23:15:50 +0200

    Semi-complete
    Semi-complete

---
+++ 12th-Set-Day-12/Exercise80.py
@@ -0,0 +1,3 @@
+#Ask the user to enter their first name and then display the length of their first name. Then ask for their surname and display the length of their surname. Join their first name and surname together with a space between and display the result. Finally, display the length of their full name (including the space). 
+
+list = [[2,5,8],[3,7,4],[1,6,9],[4,2,0]]
---
+++ 12th-Set-Day-12/Exercise81.py
@@ -0,0 +1,9 @@
+#Ask the user to type in their favourite school subject. Display it with “-” after each letter, e.g. S-p-a-n-i-s-h-.
+
+list = [[2,5,8],[3,7,4],[1,6,9],[4,2,0]]
+
+row = int(input("Select a row: "))
+
+column = int(input("Select a column: "))
+
+print(list[row][column])
